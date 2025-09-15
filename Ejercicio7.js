812d318004e4042d7a83cce4b7ca8558f616fb69
celeste-guzman <elizabethguzman.0498@gmail.com>
2025-09-15 15:34:40 -0300

    Ejercicio 7

---
+++ Ejercicio7.js
@@ -0,0 +1,11 @@
+//Ejercicio 7: Verificar si una lista contiene un elemento 
+//Tienes una lista de comidas ["Pizza", "Hamburguesa", "Tacos"]. Verifica si 
+//"Sushi" está en la lista y muestra un mensaje que confirme si está o no. 
+
+let comidas = ['Pizza', 'Hamburguesa', 'Tacos'];
+
+if (comidas.includes('Sushi')) {
+    console.log('Sushi están en la lista de comidas.');
+} else {
+    console.log('Sushi no está en la lista de comidas.');
+}
